92430aabd1b51f896f73edc790bbf7ea86fa4a26
juanm <juanmacastro1000@gmail.com>
2026-03-03 09:58:45 -0500

    Cambios JS

--- main.js
+++ main.js
@@ -1,16 +1,28 @@
-document.addEventListener('DOMContentLoaded', function() {
 
-    innertext = document.getElementsByClassName('infoContainer');
-    innertext[0].innerHTML = "Hello World";
 
+
+document.addEventListener('DOMContentLoaded', function () {
+    fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php")
+        .then(response => response.json())
+        .then(data => {
+            const coctel = document.getElementsByClassName("infoContainer");
+            coctel[0].innerHTML = `<h2>${data.drinks[0].strDrink}</h2>`;
+            coctel[0].innerHTML += `<img src="${data.drinks[0].strDrinkThumb}" alt="${data.drinks[0].strDrink}">`;
+            coctel[0].innerHTML += `<p><strong>Instructions:</strong> ${data.drinks[0].strInstructions}</p>`;
+            coctel[0].innerHTML += `<p><strong>Ingredients:</strong></p>`;
+            for (let i = 1; i <= 15; i++) {
+                if (data.drinks[0][`strIngredient${i}`]) {
+                    coctel[0].innerHTML += `<p>${data.drinks[0][`strMeasure${i}`] || 'To taste '}${data.drinks[0][`strIngredient${i}`]}</p>`;
+                
+                }
+            }
+
+            coctel[0].innerHTML += `<p><strong>ID:</strong> ${data.drinks[0].idDrink}</p>`;
+            coctel[0].innerHTML += `<p><strong>Categoria:</strong> ${data.drinks[0].strCategory}</p>`;
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+        });
 });
 
 
-fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php")
-  .then(response => response.json())
-  .then(data => {
-    console.log(data);
-  })
-  .catch(error => {
-    console.error('Error fetching data:', error);
-  });

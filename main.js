7f3fdb2bfd53bdd4ad56163ce8d4d9c8afcc838d
DarkFallenStar <kennykaven@gmail.com>
2026-03-03 12:15:20 -0500

    ajustando lo visual

--- main.js
+++ main.js
@@ -6,19 +6,16 @@ document.addEventListener('DOMContentLoaded', function () {
         .then(response => response.json())
         .then(data => {
             const coctel = document.getElementsByClassName("infoContainer");
-            coctel[0].innerHTML = `<h2>${data.drinks[0].strDrink}</h2>`;
-            coctel[0].innerHTML += `<img src="${data.drinks[0].strDrinkThumb}" alt="${data.drinks[0].strDrink}">`;
-            coctel[0].innerHTML += `<p><strong>Instructions:</strong> ${data.drinks[0].strInstructions}</p>`;
+            coctel[0].innerHTML = `<div class="title"><h2>${data.drinks[0].strDrink} <strong>ID:</strong> ${data.drinks[0].idDrink}</h2></div>`;
+            coctel[0].innerHTML += `<div class="image"><img src="${data.drinks[0].strDrinkThumb}" alt="${data.drinks[0].strDrink}"></div>`;
+            coctel[0].innerHTML += `<div class="info"><p><strong>Categoria:</strong> ${data.drinks[0].strCategory}</p>`;
             coctel[0].innerHTML += `<p><strong>Ingredients:</strong></p>`;
             for (let i = 1; i <= 15; i++) {
-                if (data.drinks[0][`strIngredient${i}`]) {
-                    coctel[0].innerHTML += `<p>${data.drinks[0][`strMeasure${i}`] || 'To taste '}${data.drinks[0][`strIngredient${i}`]}</p>`;
-                
-                }
+              if (data.drinks[0][`strIngredient${i}`]) {
+                coctel[0].innerHTML += `${data.drinks[0][`strMeasure${i}`] || 'To taste '}${data.drinks[0][`strIngredient${i}`]}`;
+              }
             }
-
-            coctel[0].innerHTML += `<p><strong>ID:</strong> ${data.drinks[0].idDrink}</p>`;
-            coctel[0].innerHTML += `<p><strong>Categoria:</strong> ${data.drinks[0].strCategory}</p>`;
+            coctel[0].innerHTML += `<div class="instruction"><p><strong>Instructions:</strong> ${data.drinks[0].strInstructions}</p></div>`;
         })
         .catch(error => {
             console.error('Error fetching data:', error);
